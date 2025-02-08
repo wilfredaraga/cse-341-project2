3fee3de7b3498ff9d33886b158ded2ed1386ad3e
wilfredaraga <wilfredeiron@gmail.com>
2025-02-08 23:53:41 +0800

    last commit

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 app.use('/', require('./routes'))
 
 
